d68affcce6465020ec1e80de94bcc27d2d69071d
Givemeboga <youssef.ben.chaouacha@gmail.com>
2023-08-18 20:03:53 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+  const toTopBtn = document.querySelector("#to-top");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("navbar-sticky");
+      toTopBtn.classList.add("show");
+    } else {
+      navbar.classList.remove("navbar-sticky");
+      toTopBtn.classList.remove("show");
+    }
+  });
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function IncrementStats() {
+  const counters = document.querySelectorAll(".counter");
+
+  counters.forEach((counter) => {
+    counter.innerText = 0;
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute("data-target");
+      const c = +counter.innerText;
+
+      const increment = target / 200;
+
+      if (c < target) {
+        counter.innerText = Math.ceil(c + increment);
+        setTimeout(updateCounter, 1);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCounter();
+  });
+}
+
+// Evennt Listeners
+document.addEventListener("DOMContentLoaded", userScroll);
+document.addEventListener("DOMContentLoaded", IncrementStats);
+document.querySelector("#to-top").addEventListener("click", scrollToTop);
